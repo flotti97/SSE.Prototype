d62fe622ed418354b493f4d250e1455b04578a91
florian.schwarzmayr <florian.schwarzmayr@gmail.com>
2026-01-17 20:59:42 +0100

    feat: improve BasicScheme Benchmarking

--- SSE.Benchmark/Benchmarks/BasicSchemeBenchmarks.cs
+++ SSE.Benchmark/Benchmarks/BasicSchemeBenchmarks.cs
@@ -9,24 +9,28 @@ using SSE.Server;
 namespace SSE.Benchmark.Benchmarks
 {
     [ShortRunJob]
+    [WarmupCount(1)]
+    [IterationCount(1)]
     public class BasicSchemeBenchmarks
     {
         private Database<(string Id, string Content)> _database;
         private byte[] _masterKey;
         private EncryptedStorageServer _server;
 
-        [Params(10, 50, 100)] 
+        [Params(10, 100, 1000, 10000)] 
         public int DocumentCount;
 
         [GlobalSetup]
         public void Init()
         {
-            string documentsPath = BenchmarkHelper.GetTestDocumentsPath();
-            var files = Directory.GetFiles(documentsPath)
+            string basePath = "/home/florian/Documents/01_Studium/Bachelorarbeit/SSE.Prototype/test_documents/20news-18828";
+            var topics = Directory.EnumerateDirectories(basePath);
+
+            var files = topics
+                .SelectMany(topic => Directory.EnumerateFiles(Path.Combine(basePath, topic)))
                 .Take(DocumentCount)
                 .Select(file => (Path.GetFileName(file), File.ReadAllText(file)))
                 .ToList();
-
 
             _database = new Database<(string, string)>(files, x => x.Item1, x => x.Item2);
 
